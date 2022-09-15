3332b5e88f4736f4f9160815448e82febd5d3531
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-15 16:06:20 +0400

    Update if-else.cs

--- if-else.cs
+++ if-else.cs
@@ -6,7 +6,29 @@ namespace myFirstProject
     {
         static void Main()
         {
-             
+             //If else
+            int a = 5;
+            bool isHasCar = false;
+
+            if (a > 7)
+            
+                Console.WriteLine("number > 7");
+             else if(a < 5)
+            
+                Console.WriteLine("number is < 5");
+            
+              else if(a == 5)
+            {
+                Console.WriteLine("number is 5");
+               // if(isHasCar)
+                    if((a == 5 || isHasCar) && a > 2)
+                    Console.WriteLine("User has a car!");
+                
+            }
+                else
+            {
+                Console.WriteLine("number is not 5");
+            };
       }
     }
 }
