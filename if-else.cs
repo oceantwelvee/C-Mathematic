50ce2214156e7ba133a410c17768d8fa71912fad
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-15 16:35:08 +0400

    Update if-else.cs

--- if-else.cs
+++ if-else.cs
@@ -29,6 +29,35 @@ namespace myFirstProject
             {
                 Console.WriteLine("number is not 5");
             };
+            
+            // Пример
+            
+            Console.Write("Enter name: ");
+            string role = Console.ReadLine();
+
+            if(role == "Admin")
+            {
+                Console.Write("Enter user name: ");
+                string user_name = Console.ReadLine();
+                Console.Write("Enter {0} age :", user_name);
+                short age = Convert.ToInt16(Console.ReadLine());
+
+                if (age <= 0 || age > 99)
+                {
+                    Console.Write("Enter {0} age :", user_name);
+                    age = Convert.ToInt16(Console.ReadLine());
+                }
+
+                if (age <= 0 || age > 99)
+                    Console.WriteLine("Error");
+                else
+                    Console.WriteLine("User's age is " + age);
+
+            } else
+            {
+                Console.WriteLine("You are not admin!");
+            }
+
       }
     }
 }
