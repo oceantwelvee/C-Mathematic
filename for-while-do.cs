3329d217c3af8ef6645d29964d81fee86616b325
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-17 12:49:43 +0400

    Update for-while-do.cs

--- for-while-do.cs
+++ for-while-do.cs
@@ -7,7 +7,49 @@ namespace myFirstProject
         static void Main()
         {
            //for while do while;
+            
+            for(byte i = 0; i <= 10; i++) {
+                Console.WriteLine("Element {0}" , i};
+                 // 0,1 ,2 ,3, 4, 5, 6,7,8,9,10
+            }
+                                  
+              for(byte i = 200; i > 10; i /=2) // Для деления /=2
+            {
+                Console.WriteLine("Element {0} ", i);
+                // 200, 100, 50, 25, 12
+            }
+                                  
+            for(byte i = 5; i <= 100; i *=2) // Для умножения *=2
+            {
+                Console.WriteLine("El {0}", i);
+                //10, 20 , 40 , 80 
+            }
+                                  
+              //While() внутри круглых скобок указываем условие
+                                  
+            byte i = 10;
 
+            while(i >= 1)
+            {
+                Console.WriteLine("Element {0}", i);
+                i--;
+                // 10,9,8,7,6,5,3,2,1
+            }
+                                   
+            bool isHasCar = true;
+
+            while(isHasCar)
+            {
+                string end = Console.ReadLine();
+                if(end == "end")
+                    isHasCar = false;
+            }   
+                                  
+               byte i = 100;
+            do
+            {
+                Console.WriteLine("El {0}" + i);
+            } while (i < 10);
 
         }
     }
