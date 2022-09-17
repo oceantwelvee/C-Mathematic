6e8759367c40e5c6e5757f72247db36b1e230c46
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-17 11:38:50 +0400

    Create Switch-Case.cs
    Switch(){
    case: some:
    some;
    break;
    }

---
+++ Switch-Case.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace myFirstProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            short user_input = Convert.ToInt16(Console.ReadLine());
+            
+            switch(user_input)
+            {
+                case 15:
+                    Console.WriteLine("number is 15");
+                    break;
+                case 16:
+                    Console.WriteLine("number is 16");
+                    break;
+
+                case 25:
+                    Console.WriteLine("number is 25");
+                    break;
+
+                case 100:
+                    Console.WriteLine("number is 100");
+                    break;
+            }
+        }
+    }
+}
