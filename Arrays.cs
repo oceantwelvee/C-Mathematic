ea697d5f39378bdb4bf4e582bbc2274f0142df63
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-26 11:01:00 +0400

    Update Arrays.cs

--- Arrays.cs
+++ Arrays.cs
@@ -51,7 +51,46 @@ namespace myFirstProject
             };
             nums[1, 1] = 50;
             
-           
+           // foreach метод
+                byte[] numbers = new byte[5];
+            numbers[0] = 10;
+            numbers[1] = 20;
+            numbers[2] = 30;
+            numbers[3] = 40;
+            numbers[4] = 50;
+
+            string[] users = new string[5];
+            users[0] = "Tamerlan";
+            users[1] = "Kamran";
+            users[2] = "Almaz";
+            users[3] = "Intigam";
+            users[4] = "Credo Bank";
+
+            for(int i = 0; i < users.Length; i++)
+            {
+                //Console.WriteLine("users : "+ users[i]);
+            }
+
+            foreach(string i in users)
+            {
+                //Console.WriteLine("users: " + i);
+            }
+
+            string[] ourCompanie = new string[10];
+            ourCompanie[0] = "Apple";
+            ourCompanie[1] = "Meta";
+            ourCompanie[2] = "Google";
+            ourCompanie[3] = "Amazon";
+            ourCompanie[4] = "Microsoft";
+            ourCompanie[5] = "HP";
+            ourCompanie[6] = "Dell";
+            ourCompanie[7] = "Asus";
+            ourCompanie[8] = "Samsung";
+            ourCompanie[9] = "Toshiba";
+
+            foreach (string com in ourCompanie)
+                Console.WriteLine("Our Companies: " + com);
+        }
             }
             
         }
