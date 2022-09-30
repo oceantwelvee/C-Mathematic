919690ecdc7e0a41a148337d36d50df3a5dd57ee
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-30 10:39:18 +0400

    Create try-catch.cs

---
+++ try-catch.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+
+namespace myFirstProject
+{
+    class Program
+    {
+        static void Main()
+        {
+           //try/catch
+           
+        }
+
+    
+
+    
+    }
+ 
+}
