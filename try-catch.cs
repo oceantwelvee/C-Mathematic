1d7dcb72037672619331348fc7803b282fa409a4
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-09-30 11:18:02 +0400

    Update try-catch.cs

--- try-catch.cs
+++ try-catch.cs
@@ -7,9 +7,37 @@ namespace myFirstProject
     {
         static void Main()
         {
-           //try/catch
-           
+            //try/catch
+ 
+           // try
+           // {
+               // int num = Convert.ToInt32(Console.ReadLine());
+              //  Console.WriteLine(num);
+          //  }catch(FormatException)
+           // {
+                //Console.WriteLine("Вы ввели не корректно!");
+           // }
+            try
+            {
+                int a, b, res;
+                Console.Write("Введите первое число: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                res = a / b;
+                Console.WriteLine("Результат: " + res);
+            }catch(DivideByZeroException)
+            {
+                Console.WriteLine("Деление на ноль!");
+            }catch(FormatException)
+            {
+                Console.WriteLine("Вы ввели не корректно!");
+            }finally
+            {
+                Console.WriteLine("Спасибо!");
+            }
         }
+
 
     
 
