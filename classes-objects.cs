9069812ea6790d98b639608893fe449f0d220e21
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-10-03 20:25:50 +0400

    Create classes-objects.cs

---
+++ classes-objects.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace myFirstProject
+{
+    class Program
+    {
+        static void Main()
+        {
+          // Создание классов и объектов
+        }
+        
+    } 
+}
