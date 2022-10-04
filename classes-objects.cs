c00cb957c0b2f22abb437fff22fda7fa8e2e369e
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-10-04 12:53:07 +0400

    Update classes-objects.cs

--- classes-objects.cs
+++ classes-objects.cs
@@ -7,6 +7,27 @@ namespace myFirstProject
         static void Main()
         {
           // Создание классов и объектов
+               Robot bot = new Robot();
+            bot.setValues("bob", 800, new byte[] {0,0,0});
+            bot.printValues();
+
+            //bot.name = "Bob";
+            //bot.weight = 800;
+            //bot.coordinates = new byte[] { 0, 0, 0 };
+
+            //Console.WriteLine(bot.name + " weight: " + bot.weight);
+
+            Robot killer = new Robot();
+            killer.setValues("Terminator", 1000, new byte[] { 0, 0, 10 });
+            killer.printValues();
+
+
+
+           // killer.name = "Terminator";
+           // killer.weight = 1000;
+           // killer.coordinates = new byte[] { 0, 0, 10 };
+
+           // Console.WriteLine(killer.name + " weight: " + killer.weight);
         }
         
     } 
