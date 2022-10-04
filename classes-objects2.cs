f989e2a960a56be2d605e0c01385def768e25119
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-10-04 12:54:33 +0400

    Create classes-objects2.cs
    Создаем класс в другом файле

---
+++ classes-objects2.cs
@@ -0,0 +1,28 @@
+using System;
+
+
+namespace project
+{
+     class Robot
+    {
+        private string name;
+
+        private int weight;
+
+        private byte[] coordinates;
+
+        public void setValues(string _name, int _weight, byte[] _coordinates)
+        {
+            name = _name;
+            weight = _weight;
+            coordinates = _coordinates;
+        }
+
+        public void printValues()
+        {
+            Console.WriteLine(name + " weight: " + weight + ". Coordinates: ");
+            foreach (byte el in coordinates)
+                Console.WriteLine(el);
+        }
+    }
+}
