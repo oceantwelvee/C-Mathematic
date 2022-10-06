8d479e1d0b464ce4c7e11818ce0c8806b97ca81b
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-10-06 09:30:00 +0400

    Update classes-objects2.cs

--- classes-objects2.cs
+++ classes-objects2.cs
@@ -26,3 +26,70 @@ namespace project
         }
     }
 }
+
+// Аксессоры get set
+
+using System;
+
+
+namespace project
+{
+    class Robot
+    {
+        private static int count;
+
+        private string name;
+        private int weight;
+        private byte[] coordinates;
+
+        public int Weight
+        {
+            get
+            {
+                Console.Write("Результат: ");
+                return this.weight;
+            }
+            set
+            {
+                if (value < 1)
+                    this.weight = 1;
+                else if (value > 5000)
+                    this.weight = 5000;
+                else
+                    this.weight = value;
+            }
+        }
+
+        public Robot(string name, int weight, byte[] coordinates)
+        {
+            Console.WriteLine("Object has been created");
+            this.setValues(name, weight, coordinates);
+            count++;
+        }
+
+        public Robot() { 
+            count++;
+
+        }
+        public void setValues(string name, int weight, byte[] coordinates)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.coordinates = coordinates;
+        }
+
+        public void printValues()
+        {
+            Console.WriteLine(this.name + " weight: " + this.weight + " .Coordinates");
+            foreach(byte el in this.coordinates)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("count is: " + count);
+        }
+    }
+}
